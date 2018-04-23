ee4df8b04c61d7f6c22458c51a73f6d802d3b7ce
fcarlone <frankcarlone@gmail.com>
2018-04-23 15:39:49 -0400

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,43 @@
+// const MongoClient = require('mongodb').MongoClient;
+
+// es6 Object Destructuring
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+// var user = {name: 'Frank', age: 46};
+// var {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // Insert new doc into Users (name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Frank Carlone',
+  //   age: 46,
+  //   location: 'Philadelphia'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //   // console.log(JSON.stringify(result.ops, undefined, 2));
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
+
+  db.close();
+});
