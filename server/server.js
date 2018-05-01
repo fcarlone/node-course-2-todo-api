53646bdcb592dd555ca4113866256119d1536828
fcarlone <frankcarlone@gmail.com>
2018-05-01 10:57:32 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require ('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -30,6 +31,28 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET /todos/1234
+app.get('/todos/:id', (req, res) => {
+  // res.send(req.params);
+  var id = req.params.id;
+
+  // Valid id using isValid
+    // 404 - send back empty send
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  };
+  // findById
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      // if no todo - send back 404 with empty body
+      return res.status(404).send();
+    }
+      // if todo - send it back
+      res.send({todo});
+  //  error 400 - and send empty body back
+  }).catch((e) => res.status(400).send());
 });
 
 app.listen(3000, () => {
