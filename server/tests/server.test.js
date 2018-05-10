08ec96ccc8393ec9fc8802d95f2fb64494b396c8
fcarlone <frankcarlone@gmail.com>
2018-05-10 14:53:05 -0400

    Update server-test suite

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -188,6 +188,7 @@ describe('PATCH /todos/:id', () => {
         expect(res.body.todo.text).toBe(text)
         expect(res.body.todo.completed).toBe(true)
         // expect(res.body.todo.completedAt).toBeType('number');
+        expect(typeof res.body.todo.completedAt).toBe('number');
       })
       .end(done)
   });
@@ -273,7 +274,7 @@ describe('POST /users', () => {
 
       User.findOne({email}).then((user) => {
         expect(user).toBeTruthy();
-        // expect(user.password).toBeFalsy(password);
+        expect(user.password).not.toBe(password);
         done();
       }).catch((e) => done(e));
     });
@@ -314,7 +315,7 @@ describe('POST /users/login', () => {
         }
 
         User.findById(users[1]._id).then((user) => {
-          expect(user.tokens[1]).toMatchObject({
+          expect(user.toObject().tokens[1]).toMatchObject({
             access: 'auth',
             token: res.headers['x-auth']
           });
@@ -332,7 +333,7 @@ describe('POST /users/login', () => {
     })
     .expect(400)
     .expect((res) => {
-      expect(res.headers['x-auth']).toBeFalsy
+      expect(res.headers['x-auth']).toBeFalsy();
     })
     .end((err, res) => {
       if (err) {
